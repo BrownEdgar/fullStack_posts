390735a7457385e4bf16d87f3392e039c3134378
BrownEdgar <nikoghosjanedgar@gmail.com>
2024-05-09 15:56:43 +0400

    created user models

---
+++ server/models/user.model.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: [true, "this email is alredy exist!"]
+  },
+  age: {
+    type: Number,
+    required: true,
+    min: 18,
+    max: 99
+  },
+  password: {
+    type: String,
+    required: true,
+    validate: (value) => {
+      const startWithUppercase = /^[A-Z]/.test(value);
+      const hasNumber = /\d/.test(value);
+      if (startWithUppercase && hasNumber && value.length > 6) {
+        return true
+      }
+    },
+    message: "invalid password"
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = model('user', userSchema)
